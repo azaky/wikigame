0f0d0aa084053d3b16fb1987151d9e55ecf7b726
Ahmad Zaky <ahmadzaky003@gmail.com>
2020-09-14 08:36:30 +0700

    add production server url

--- extension/background.js
+++ extension/background.js
@@ -12,7 +12,9 @@ var h,f,l,d=String.fromCharCode;t.exports={version:"2.1.2",encode:a,decode:p}},f
 function isObject(item){return (item && typeof item === 'object' && !Array.isArray(item));}
 function mergeDeep(target,...sources){if(!sources.length)return target;const source=sources.shift();if(isObject(target)&&isObject(source)){for(const key in source){if(isObject(source[key])){if(!target[key])Object.assign(target,{[key]:{}});mergeDeep(target[key],source[key]);}else{Object.assign(target,{[key]:source[key]});}}}return mergeDeep(target,...sources);}
 
-let serverUrl = 'http://localhost:9454'
+let serverUrl = 'https://wikigame-multiplayer.herokuapp.com/'
+// for development:
+// let serverUrl = 'http://localhost:9454'
 
 let active = false;
 let socket;
