13fd2dc7586c87dd34697d48fa0ddaf1e98a7e12
Ahmad Zaky <ahmadzaky003@gmail.com>
2020-10-14 21:42:36 +0700

    reactify article autocomplete

--- extension/src/wiki.js
+++ extension/src/wiki.js
@@ -3,3 +3,25 @@ export function getRandomPage() {
     .then(res => res.json())
     .then(data => data.items && data.items.length && data.items[0].title);
 }
+
+export function getAutocomplete(keyword) {
+  const host = 'https://en.wikipedia.org';
+  const endpoint = '/w/api.php';
+  const url = new URL(endpoint, host);
+
+  // API Spec: https://en.wikipedia.org/w/api.php?action=help&modules=opensearch
+  let queryParams = {
+    action: 'opensearch',
+    format: 'json',
+    profile: 'fuzzy', // Supporting Typo
+    search: keyword,
+    limit: 5,
+  };
+  Object.keys(queryParams).forEach(key =>
+    url.searchParams.append(key, queryParams[key])
+  );
+
+  return window.fetch(url)
+    .then(res => res.json())
+    .then(data => data[1]);
+}
