a36e17fb1b6f57cfb54a0ae880241df4a0a71ce4
Ahmad Zaky <ahmadzaky003@gmail.com>
2020-10-14 09:46:28 +0700

    fix versioning -- drop -beta on manifest.json

--- extension/scripts/version.js
+++ extension/scripts/version.js
@@ -6,10 +6,12 @@ console.log('Running version tagging..');
 try {
   const version = process.argv[2];
   console.log('detected version:', version);
+  const extVersion = version.replace('-beta', '');
+  console.log('tagging version:', extVersion);
 
   const manifestPath = path.join(__dirname, '../dist/manifest.json');
   let manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
-  Object.assign(manifest, {version});
+  Object.assign(manifest, {version: extVersion});
   fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2), 'utf8');
 } catch (e) {
   console.error('version tagging error:', e);
