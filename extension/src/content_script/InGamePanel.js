fe3875c871936ee5847d5bd68292640de1e39f9e
Ahmad Zaky <ahmadzaky003@gmail.com>
2020-10-16 02:06:46 +0700

    restore rules (clicks, ctrlf, hidden elements) after game is finished

--- extension/src/content_script/InGamePanel.js
+++ extension/src/content_script/InGamePanel.js
@@ -11,68 +11,100 @@ export function InGamePanel(props) {
   useEffect(() => {
     if (currentState.finished) return;
 
-    // remove search bar
-    document.getElementById('simpleSearch').remove();
+    const hiddenElements = [
+      'simpleSearch', // search bar
+      'ca-viewsource', // View Source
+      'ca-edit', // Edit
+      'ca-history', // View History
+    ];
+    hiddenElements.forEach(id => {
+      const el = document.getElementById(id);
+      if (el) {
+        el.style.display = 'none';
+      }
+    });
 
     // ctrl+f
+    let handleCtrlf;
     if (typeof rules.allowCtrlf === 'boolean' && !rules.allowCtrlf) {
-      window.addEventListener('keydown', e => {
+      handleCtrlf = e => {
         if ((e.ctrlKey || e.metaKey) && (e.key === 'f' || e.key === 'F')) {
           e.preventDefault();
           toast.error('Oops, Ctrl+F is not allowed!', {toastId: 'ctrlf'});
         }
-      });
+      }
+      window.addEventListener('keydown', handleCtrlf);
     }
+
+    return () => {
+      // restore hidden elements
+      hiddenElements.forEach(id => {
+        const el = document.getElementById(el);
+        if (el) {
+          el.style.display = 'block';
+        }
+      });
+
+      if (handleCtrlf) {
+        window.removeEventListener('keydown', handleCtrlf);
+      }
+    };
   }, [currentState.finished]);
 
   // override a.click
   useEffect(() => {
-    const links = document.getElementsByTagName('a');
     if (currentState.finished) return;
 
-    for (let i = 0; i < links.length; i++) {
-      links[i].onclick = (function (target) {
-        return function (e) {
-          const link = target.href;
-          console.log('Clicking:', link);
+    const clickHandler = function (e) {
+      if (e.target.tagName !== 'A') return;
 
-          const articleObj = util.getArticleFromUrl(link);
-          const { article } = articleObj;
+      const link = e.target.href;
 
-          // anchor links
-          if (article === util.getCurrentArticle()) {
-            console.log(`Anchor link, doesn't count as a click:`, link);
-            return;
-          }
+      const articleObj = util.getArticleFromUrl(link);
+      const { article } = articleObj;
 
-          e.preventDefault();
+      // anchor links
+      if (article === util.getCurrentArticle()) {
+        console.log(`Anchor link, doesn't count as a click:`, link);
+        return;
+      }
 
-          // non-wiki links
-          if (!article) {
-            console.log('Ignoring invalid links:', link);
-            return;
-          }
+      e.preventDefault();
 
-          // special links
-          if (util.isSpecialArticle(article)) {
-            console.log('Ignoring special links:', link);
-            return;
-          }
+      // non-wiki links
+      if (!article) {
+        toast.error(`You cannot go outside Wikipedia!`);
+        console.log('Ignoring invalid links:', link);
+        return;
+      }
 
-          console.log('Navigating to:', article);
+      // special links
+      if (util.isSpecialArticle(article)) {
+        toast.error(`It's a special Wikipedia link, you cannot go there!`);
+        console.log('Ignoring special links:', link);
+        return;
+      }
 
-          chrome.storage.local.get(['localState'], ({localState}) => {
-            if (localState === 'clicking') {
-              console.log(`Ignoring clicks, there's another ongoing clicking event`);
-              return;
-            }
-            chrome.storage.local.set({localState: 'clicking'}, () => {
-              util.goto(article);
-            });
-          });
-        };
-      }(links[i]));
-    }
+      console.log('Navigating to:', article);
+
+      chrome.storage.local.get(['localState'], ({localState}) => {
+        if (localState === 'clicking') {
+          console.log(`Ignoring clicks, there's another ongoing clicking event`);
+          return;
+        }
+        chrome.storage.local.set({localState: 'clicking'}, () => {
+          util.goto(article);
+        });
+      });
+    };
+
+    document.addEventListener('click', clickHandler);
+
+    return () => {
+      if (clickHandler) {
+        document.removeEventListener('click', clickHandler);
+      }
+    };
   }, [currentState.finished]);
 
   return (
