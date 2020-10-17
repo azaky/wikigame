b841fd5c53b3d71c232e5489d696a95de9e47199
Erlangga Wulung <erlanggawulung@gmail.com>
2020-10-18 00:41:28 +0700

    remove unused files

--- extension/static/popup.js
+++ extension/static/popup.js
@@ -1,26 +1,3 @@
-// function hello() {
-//   var roomId = document.getElementById('roomId').value;
-//   var username = document.getElementById('username').value;
-//   var data = {
-//     roomId,
-//     username
-//   }
-//   chrome.tabs.executeScript({
-//     code: "var data = JSON.parse('" + encodeToPassToContentScript(data) + "'); var username"
-//   }, function () {
-//       chrome.tabs.executeScript({
-//           file: "popupExecute.js"
-//       });
-//   });
-// }
-
-// function encodeToPassToContentScript(obj){
-//   // Encodes into JSON and quotes \ characters so they will not break
-//   // when re-interpreted as a string literal. Failing to do so could
-//   // result in the injection of arbitrary code and/or JSON.parse() failing.
-//   return JSON.stringify(obj).replace(/\\/g,'\\\\').replace(/'/g,"\\'")
-// }
-
 document.getElementById('startButton').addEventListener('click', function() {
   var roomId = document.getElementById('roomId').value;
   var username = document.getElementById('username').value;
